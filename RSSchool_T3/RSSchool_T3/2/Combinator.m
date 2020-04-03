700d79d6cf0719ae65421c5a311eb675fafed69c
Anjey Novicki <anjeynov@gmail.com>
2020-04-03 12:48:44 +0300

    feat: Part 2 done

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,37 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    NSUInteger c = [[array firstObject] intValue];
+    NSUInteger n = [[array lastObject] intValue];
+    NSUInteger result = 2;
+    
+    if (n == 1) {
+        return [array firstObject];
+    }
+    
+    if (c == n) {
+        return [[NSNumber alloc] initWithInt:1];
+    }
+   
+    NSUInteger factN = [Combinator factorial:n];
+    
+    while (result < n) {
+        NSUInteger tmp = factN / ( [Combinator factorial:(n - result)] * [Combinator factorial:result]);
+        if(tmp == c) {
+            return [[NSNumber alloc] initWithInt: (int)result];
+        }
+        result += 1;
+    }
+    
+    return nil;
 }
+
++ (NSUInteger)factorial:(NSUInteger) n {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * [Combinator factorial:(n - 1)];
+    }
+}
+
 @end
